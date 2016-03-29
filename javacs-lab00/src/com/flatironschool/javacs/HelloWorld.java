ecbad8f8cade4af019db48d38a58f27dd52bdcf3
Megan <Megan@macbook-pro-105.ucsd.edu>
2016-03-28 23:50:22 -0700

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,11 +1,22 @@
 package com.flatironschool.javacs;
 
+//Megan Wang CodeU 2016
+
 public class HelloWorld {
+    
+    /**
+     * Method that returns the version number of the system
+     */
+
     public static Double getVersion() {
-        // I won't work!
+        //Create a string of the version number
+        String versionNum = System.getProperty("java.specification.version");
+        
+        //Return the double version of the string
+        return Double.parseDouble(versionNum);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+    
     }
 }
